b580a98e058e781e960f3ac45640da23eec54ccc
HarisR39 <fireplatypus375@gmail.com>
2023-10-15 15:58:14 -0400

    created databse functionality

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 import customtkinter as ctk
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://fireplatypus375:0TgN3YyiObPpHtmQ@fblamain.emmytgc.mongodb.net/")
+db = cluster["FBLAMain"]
+login_info = db["login_info"]
+
 
 root= ctk.CTk()
 root.geometry("1000x500+100+100")
