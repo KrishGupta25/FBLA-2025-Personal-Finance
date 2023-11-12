0c3da05947b6c137c00076a0a9ce2dfa9fd29591
HarisR39 <fireplatypus375@gmail.com>
2023-11-12 00:02:00 -0500

    font fix

--- src/main.py
+++ src/main.py
@@ -2,22 +2,31 @@
 import tkinter as tk
 import customtkinter as ctk
 from pymongo import MongoClient
-from tkextrafont import Font
+import pyglet
 
 #set up connections with the database
 cluster = MongoClient("mongodb+srv://fireplatypus375:0TgN3YyiObPpHtmQ@fblamain.emmytgc.mongodb.net/")
 db = cluster["FBLAMain"]
-login_info = db["login_info"]
+loginInfo = db["login_info"]
 
+#import custom font
+pyglet.font.add_file('assets/Quicksand-Bold.ttf')
+
+#creates main home gui
 root= ctk.CTk()
 root.geometry("1000x500+100+100")
 
-font = Font(file="./assets/Quicksand-Bold.ttf", family="Quicksand")
 
-login_label = ctk.CTkLabel(root, text="login", font=(font,50))
-login_label.place(relx=0.5, rely=0.2, anchor="center")
+loginLabel = ctk.CTkLabel(root, text="login", font=("Quicksand",50))
+loginLabel.place(relx=0.5, rely=0.2, anchor="center")
+
+def done():
+    loginInfo.insert_one()
+randomButton = ctk.Ctkbutton(root, text="click me", command= done, )
+randomButton.place(relx= 0.5, rely= 0.4)
 
 
+#keeps application running 
 if __name__ == "__main__":
     root.mainloop()
 
