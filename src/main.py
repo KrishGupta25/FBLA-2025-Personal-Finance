f68a2bf93370416cc42cac814bf40514499b76f2
HarisR39 <fireplatypus375@gmail.com>
2023-11-12 00:08:22 -0500

    fixed font

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import tkinter as tk
 import customtkinter as ctk
 from pymongo import MongoClient
-from tkextrafont import Font
+import pyglet
 
 #set up connections with the database
 cluster = MongoClient("mongodb+srv://fireplatypus375:0TgN3YyiObPpHtmQ@fblamain.emmytgc.mongodb.net/")
@@ -19,9 +19,8 @@ root= ctk.CTk()
 root.geometry("1000x500+100+100")
 
 
-font = Font(file="./assets/Quicksand-Bold.ttf", family="Quicksand")
 
-login_label = ctk.CTkLabel(root, text="login", font=(font,50))
+login_label = ctk.CTkLabel(root, text="login", font=("Quicksand",50))
 login_label.place(relx=0.5, rely=0.2, anchor="center")
 
 
