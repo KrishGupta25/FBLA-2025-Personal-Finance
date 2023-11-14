7553a39c5d6cf95f58b34de956df490c32d7a2da
HarisR39 <fireplatypus375@gmail.com>
2023-11-14 16:37:11 -0500

    test file

---
+++ src/temp.py
@@ -0,0 +1,7 @@
+for a in range(1,7):
+    for b in range(1,7):
+        for c in range(1,7):
+            for d in range(1,7):
+                if a+b+c+d == 13:
+                    print(f'{a},{b},{c},{d}')
+    
