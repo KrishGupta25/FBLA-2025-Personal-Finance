e7cd821a53905f88fff57dc20d9359c9330c2069
HarisR39 <fireplatypus375@gmail.com>
2023-12-30 16:42:16 -0500

    stuff

--- src/main.py
+++ src/main.py
@@ -27,15 +27,43 @@ def font(size):
 #Creates main home gui
 root = ctk.CTk(fg_color = color) 
 root.geometry("1200x600+180+120")
+loginFrame = ctk.CTkFrame(root, width = 1200, height = 600, fg_color = color)
+loginFrame.place(relx = 0, rely = 0)
 
 
 #Creates login to xxxx label
-loginLabel = ctk.CTkLabel(root, text="Login to xxxx", font=font(50))
+loginLabel = ctk.CTkLabel(loginFrame, text="Login to xxxx", font=font(50))
 loginLabel.place(relx=0.5, rely=0.2, anchor="center")
 
+usernameLabel = ctk.CTkLabel(loginFrame, text="Email", font=font(15))
+usernameLabel.place(relx=0.335, rely=0.36, anchor="nw")
+usernameEntry = ctk.CTkEntry(loginFrame, font= font(15), placeholder_text= "Name@domain.com", width= 400, height= 40, justify= "center", show= "*")
+usernameEntry.place(relx= 0.5, rely= 0.45, anchor= "center")
+
+passLabel = ctk.CTkLabel(loginFrame, text="Password", font=font(15))
+passLabel.place(relx=0.335, rely=0.51, anchor="nw")
+passEntry = ctk.CTkEntry(loginFrame, font= font(15), placeholder_text= "Password", width= 400, height= 40, justify= "center", show= "*")
+passEntry.place(relx= 0.5, rely= 0.6, anchor= "center")
+
+
+def login():
+    logins = list()
+    logins = loginInfo.find()
+    for items in logins:
+        if usernameEntry.get() == items['email'] and passEntry.get() == items['password']:
+            print("hello")
+
+
+#Creates log in up button
+loginButton = ctk.CTkButton(loginFrame, text="Login in to xxxx", font=font(25), command= login, fg_color=color)
+loginButton.place(relx=0.5, rely=0.75, anchor="center")
+
 #Creates sign up button
-signupButton = ctk.CTkButton(root, text="Sign up for xxxx", font=font(25), command= lambda:(signUp(root)))
-signupButton.place(relx=0.5, rely=0.8, anchor="center")
+signupButton = ctk.CTkButton(loginFrame, text="Sign up for xxxx", font=font(25), command= lambda:(signUp(loginFrame)), fg_color=color)
+signupButton.place(relx=0.5, rely=0.9, anchor="center")
+
+
+
 
 
 
